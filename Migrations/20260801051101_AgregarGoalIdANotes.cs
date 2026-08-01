fbf2ac13ea1a772ef0874c617b2353eebf5a0a3c
HeidiEsther123 <heidi.pena@tecdesoftware.edu.mx>
2026-07-31 23:12:30 -0600

    feat: agregar migracion para relacion Note-Goal

---
+++ Migrations/20260801051101_AgregarGoalIdANotes.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GoodGoals.Migrations
+{
+    /// <inheritdoc />
+    public partial class AgregarGoalIdANotes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "GoalId",
+                table: "Notes",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Notes_GoalId",
+                table: "Notes",
+                column: "GoalId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Notes_Goals_GoalId",
+                table: "Notes",
+                column: "GoalId",
+                principalTable: "Goals",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Notes_Goals_GoalId",
+                table: "Notes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Notes_GoalId",
+                table: "Notes");
+
+            migrationBuilder.DropColumn(
+                name: "GoalId",
+                table: "Notes");
+        }
+    }
+}
